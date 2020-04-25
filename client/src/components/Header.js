94fe13b20f95c35270c541c49a5a99eddd85fb68
alexgignilliat <56325924+alexgignilliat@users.noreply.github.com>
2020-04-25 10:43:55 -0400

    MVC file structure setup

---
+++ client/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react"
+import "../App.css"
+
+//placeholder code
+
+function Header() {
+    return (
+        <div>
+            <p style={{ fontSize: "20px", float: "left" }}>bandwagon.</p>
+            <p style={{float: "right"}}>Bandwagon logo and
+                <br/> dropdown menu here</p>
+           
+
+            <br />
+        </div>
+
+    )
+}
+
+export default Header;
