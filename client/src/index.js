78de9546c649f97586318553c184202fa451e87a
aagrosse <aagrock81@yahoo.com>
2020-04-29 04:56:39 -0400

    purging unused files

--- client/src/index.js
+++ client/src/index.js
@@ -6,13 +6,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import AdminLayout from "./pages/Admin.jsx";
 
-
-
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/css/animate.min.css";
 import "./assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0";
-import "./assets/css/demo.css";
 import "./assets/css/pe-icon-7-stroke.css";
 
 ReactDOM.render(
