e8f0877da4b9d99f8951e8667620e504ce97e1bd
aagrosse <aagrock81@yahoo.com>
2020-05-09 09:54:46 -0400

    add catch all symbol to react route

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.use("/api", apiRoutes);
 
 
 // If no API routes are hit, send the React app
-router.use((req, res) => {
+router.use("*", (req, res) => {
   res.sendFile(
     path.join(
       __dirname,
