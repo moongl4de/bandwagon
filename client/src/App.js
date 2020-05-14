9f59be892317b29168bb88e2cbc41588a2c198ee
Daria <dariagnaumova@gmail.com>
2020-05-14 13:03:15 -0400

    artist names query the attempts

--- client/src/App.js
+++ client/src/App.js
@@ -34,22 +34,17 @@ function App() {
         <Route exact path='/' component={Login} />
         <Route exact path='/signup' component={Signup} />
         <Route exact path='/activate/:token' component={ActivateUser} />
-        <Route exact path='/test' component={Upload} />
         <ListenerPrivateRoute exact path='/listener' component={Center} />
-        {/* <Route exact path='/test' component={AWS}/> */}
         <ListenerPrivateRoute exact path='/subscription' component={Subscription} />
-        {/* <ListenerPrivateRoute exact path='/albums/:id' component={Details}/> */}
         <ArtistPrivateRoute exact path='/admin/dashboard' component={Admin}/>
         <ArtistPrivateRoute exact path='/admin/user' component={Admin}/>
         <ArtistPrivateRoute exact path='/admin/edit' component={Admin}/>
         <Route exact path='/admin/upload' component={Admin}/>
         <ArtistPrivateRoute exact path='/admin/library' component={Admin}/>
-        {/* <Route exact path='/test' component={AWS}/> */}
         <ArtistPrivateRoute exact path='/artistpage' component={ArtistPage}/>
         <ArtistPrivateRoute exact path='/artistpage' component={ArtistPage}/>
         <AdminPrivateRoute exact path='/admin/admin' component={Admin2}/>
         {/* <Route exact path='/admin/admin' component={Admin2}/> */}
-        
       </StoreProvider>
     </Router>
 
