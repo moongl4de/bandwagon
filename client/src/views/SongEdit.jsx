cde50a87d79b3f11368577bd50ee8acdb91a58f3
aagrosse <aagrock81@yahoo.com>
2020-05-20 04:13:07 -0400

    added update page functionality

--- client/src/views/SongEdit.jsx
+++ client/src/views/SongEdit.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect  } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Container,
   Row,
@@ -17,20 +17,10 @@ import { isAuth } from "../components/helper";
 
 
 function UserProfile() {
-  const array= {
+  const array = {
     data: [
-      { album: "Fight With Tools", song: "Handlebars", date: "1999", edit: "", delete: "" },
-      { album: "Grand National", song: "Daniella", date: "2001", },
-      { album: "An Awesome Wave", song: "Breezeblocks", date: "2010", },
-      { album: "In With the Old", song: "Ashes", date: "2008", },
-      { album: "ExtraOrdinary", song: "Helen", date: "2010", },
-      { album: "Food in the Belly", song: "The Letter", date: "2006", },
-      { album: "Fight With Tools", song: "Handlebars", date: "1999" },
-      { album: "Grand National", song: "Daniella", date: "2001", },
-      { album: "An Awesome Wave", song: "Breezeblocks", date: "2010", },
-      { album: "In With the Old", song: "Ashes", date: "2008", },
-      { album: "ExtraOrdinary", song: "Helen", date: "2010", },
-      { album: "Food in the Belly", song: "The Letter", date: "2006", }
+      { album: "Fight With Tools", song: "Handlebars", artist: "1999", edit: "", delete: "" },
+
     ]
   }
 
@@ -43,90 +33,98 @@ function UserProfile() {
   // const [art, setArt] = useState("test art");
   // const [artURL, setArtURL] = useState("https://i.pinimg.com/originals/20/13/ac/2013ac80f2aededf644ac3b96de44a64.jpg");
   const [page, setPage] = useState(false);
-  
+
   const [newsongs, setNewSongs] = useState([]);
   const [editSong, setEditSong] = useState([]);
-  
-  
+  const [editAlbum, setEditAlbum] = useState([]);
+  const [album, setAlbum] = useState({
+    title: "",
+    album: "",
+    description: "",
+    art: "",
+  });
+
+
   const getSongs = () => {
     const userId = isAuth()._id;
     API.getSongByUserId(userId)
       .then((results) => {
         console.log("all songs from db:", results.data);
         setNewSongs(results.data);
-     
 
 
-       
+
+
       })
       .catch((err) => console.log(err));
   };
- 
+
   useEffect(() => {
 
     getSongs();
+
+  }, []);
+
+  const deleteSongs = (song) => {
     
-  }, []); 
 
-const deleteSongs = (song) => {
-  console.log("CLICKED SONG", song);
+    const id = song._id
+    API.deleteAlbum(id)
+      .then((res) => {
+        console.log("after delete API", res)
+        getSongs()
+        toast("Your song has been deleted")
+      })
+      .catch((err) => console.log("ERROR:" + err));
 
-  const id = song._id
-  API.deleteAlbum(id)
-  .then((res) => {
-    console.log("after delete API", res)
-    getSongs()
-    toast("Your song has been deleted")
-     })
-  .catch((err) => console.log("ERROR:"+ err));
-  
-};
+  };
 
 
 
 
 
 
-function handleEdit(song) {
-  // const { albumId, title, date, _id, description} = song
-  console.log("YOYOYOYOYOY")
-  console.log(song)
-  setState({
-    title: song.title,
-    albumName: song.album["Album title"],
-    date: song.date,
-    description: song.album.description,
-    art: song.album.art
-  });
-  setEditSong(song)
+  function handleEdit(song) {
+    // const { albumId, title, date, _id, description} = song
+   
+    setState({
+      albumId: song.album._id,
+      title: song.title,
+      album: song.album.title,
+      description: song.album.description,
+      art: song.album.art
+    });
+    setEditSong(song)
+    console.log(song.albumId)
+    getAlbum(song.albumId)
 
- 
 
-  // setPage(true);
-  console.log("go to edit")
-}
+    // setPage(true);
+    console.log("go to edit")
+  }
 
-function renderTableHeader() {
-  let header = Object.keys(array.data[0])
-  return header.map((key, index) => {
-    return <th key={index}>{key.toUpperCase()}</th>
-  })
-}
+  function renderTableHeader() {
+    let header = Object.keys(array.data[0])
+    return header.map((key, index) => {
+      return <th key={index}>{key.toUpperCase()}</th>
+    })
+  }
 
-function renderTableData() {
-  return newsongs.map((song) => {
-    console.log(song)
-    return (
-      <tr key={song.albumId}>
-        <td>{song.album["Album title"]}</td>
-        <td>{song.title}</td>
-        <td>{song.date}</td>
-        <td><a style={{ cursor: "pointer", color: "orange" }} class='edit-song'onClick={() => handleEdit(song)}>EDIT</a></td>
-        <td><a style={{ cursor: "pointer", color: "red" }} class='delete-song' id={song._id} onClick={() => deleteSongs(song)}>DELETE</a></td>
-      </tr>
-    )
-  })
-}
+  function renderTableData() {
+    return newsongs.map((song) => {
+   
+
+      return (
+        <tr key={song._id}>
+          <td>{song.album.title}</td>
+          <td>{song.title}</td>
+          <td>{song.user.name}</td>
+          <td><a style={{ cursor: "pointer", color: "orange" }} class='edit-song' onClick={() => handleEdit(song)}>EDIT</a></td>
+          <td><a style={{ cursor: "pointer", color: "red" }} class='delete-song' id={song._id} onClick={() => deleteSongs(song)}>DELETE</a></td>
+        </tr>
+      )
+    })
+  }
 
   const handleInputChange = (event) => {
     setState({
@@ -134,45 +132,83 @@ function renderTableData() {
       [event.target.name]: event.target.value,
     });
   };
-  
-  
-  
-  
-  
-  
-  
-  
+
+  function getAlbum(id) {
+    console.log(id)
+    API.getAlbum(id)
+      .then((result) => {
+        setEditAlbum(result.data)
+        
+        console.log("GOT ALBUM", result.data);
+      })
+  }
+
+
+
+
+
+
+  function updateAlbumToDb() {
+    
+    let data = state
+    console.log("before update")
+    console.log(editAlbum)
+    API.updateAlbum({
+      ...data,
+      ...editAlbum,
+      title: data.album,
+      description: data.description,
+      art: data.art,
+    })
+      .then((result) => {
+        console.log("album updated!!!", result.data);
+        getSongs();
+        toast("Your song has been changed");
+        setState({
+          _id: "",
+
+          title: "",
+          album: "",
+          description: "",
+          art: ""
+        });
+      })
+  }
+
+
+
+
   const handleSubmit = e => {
     e.preventDefault();
-   
+    updateAlbumToDb();
+    
     let data = state
-    console.log("HERE")
-    console.log(state)
-   API.updateSong({ 
-    
-    ...data,
-    ...editSong,
-    title: data.title,
-       
-      })
-        .then((result) => {
-          console.log("song updated", result.data);
-          getSongs();
-          toast("Your song has been changed")
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    
-    // toast("Songs successfully edited");
+    API.updateSong({
+
+      ...data,
+      ...editSong,
+      title: data.title,
+      album:{
+        title: data.album,
+        description: data.description,
+        art: data.art
+      }
+    })
+      .then((result) => {
+
       
-      // updateSongInfo(song)
-    
-    console.log('Button is clicked!');
-    // setPage(true)
+
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+  
 
 
   }
+
+
 
 
 
@@ -211,11 +247,12 @@ function renderTableData() {
                       <Form.Label>Song Name:</Form.Label>
                       <Form.Control
                         type="text"
-                        placeholder={state.title}
+                        value={state.title}
+                        // placeholder={state.title}
                         onChange={handleInputChange}
-                        name = "title"
-                        
-                         />
+                        name="title"
+
+                      />
                     </Form.Group>
 
 
@@ -226,11 +263,12 @@ function renderTableData() {
                       <Form.Label>Album Name:</Form.Label>
                       <Form.Control
                         type="text"
-                        placeholder={state.albumName}
+
+                        value={state.album}
                         onChange={handleInputChange}
                         name="album"
-                        disabled
-                         />
+                      // disabled
+                      />
                     </Form.Group>
 
 
@@ -242,11 +280,12 @@ function renderTableData() {
                       <Form.Label>Description:</Form.Label>
                       <Form.Control
                         type="text"
-                        placeholder={state.description}
+
+                        value={state.description}
                         onChange={handleInputChange}
-                        name = "description"
-                        disabled
-                         />
+                        name="description"
+                      // disabled
+                      />
                     </Form.Group>
 
                   </Form.Row>
@@ -257,11 +296,12 @@ function renderTableData() {
                       <Form.Label>Art URL:</Form.Label>
                       <Form.Control
                         type="text"
-                        placeholder={state.art}
+
+                        value={state.art}
                         onChange={handleInputChange}
                         name="art"
-                        disabled
-                        />
+                      // disabled
+                      />
                     </Form.Group>
 
                   </Form.Row>
@@ -320,38 +360,38 @@ function renderTableData() {
 
 
 
-    
-          <Row>
-            <Col md={12}>
-              <Card
-                title="Uploaded Music"
-                category="Welcome to Your Music Library"
-                ctTableFullWidth
-                ctTableResponsive
-                content={
-                  <div>
 
-                    <Table id='students' className="ml-3" striped hover>
-                      <tbody>
-                        <tr>{renderTableHeader()}</tr>
-                        {renderTableData()}
-                      </tbody>
-                    </Table>
-                  </div>
+        <Row>
+          <Col md={12}>
+            <Card
+              title="Uploaded Music"
+              category="Welcome to Your Music Library"
+              ctTableFullWidth
+              ctTableResponsive
+              content={
+                <div>
 
-
+                  <Table id='students' className="ml-3" striped hover>
+                    <tbody>
+                      <tr>{renderTableHeader()}</tr>
+                      {renderTableData()}
+                    </tbody>
+                  </Table>
+                </div>
 
 
 
 
 
-                }
-              />
-            </Col>
 
 
-          </Row>
-        </Container>
+              }
+            />
+          </Col>
+
+
+        </Row>
+      </Container>
     </div >
   );
 }
@@ -364,3 +404,4 @@ function renderTableData() {
 
 
 export default UserProfile;
+
